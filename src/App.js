df9fcb6b6018b73fbc4e327f8614ade8ae3ae2ab
Ryan Dupont <ryanandrewdupont@gmail.com>
2025-02-27 22:36:25 -0500

    Contact Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import AboutPage from './pages/AboutPage';
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Footer from './components/Footer';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import './App.css';
 
 function App() {
@@ -12,8 +14,13 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='about' element={<AboutPage /> } />
         <Route path='contact' element={<ContactPage />} />
         <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route 
+            path='directory/:campsiteId'
+            element={<CampsiteDetailPage />}
+        />
       </Routes>
       <Footer />
     </div>

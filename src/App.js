14cb64a20158c823c016349c7f177907a07bc809
Ryan Dupont <ryanandrewdupont@gmail.com>
2025-03-14 22:29:58 -0400

    Fetch API, Fetch with useEffect(), Asynchronous Redux, Redux: createAsyncThunk, Async Campsites, Error and Loading

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import AboutPage from './pages/AboutPage';
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
@@ -6,9 +8,17 @@ import Header from './components/Header';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Footer from './components/Footer';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 import './App.css';
 
+
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <Header />
